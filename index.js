1006e2fb41be57e168b4593cad1048b114cff8fc
la105807 <laura.sierra17@knights.ucf.edu>
2021-02-07 14:34:11 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,10 @@
+let quotes = [`"All that we see or seem is but a dream within a dream."
+- Edgar Allan Poe`,
+`"The way to get started is to quit talking and begin doing." 
+- Walt Disney`,
+`"If life were predictable it would cease to be life, and be without flavor." 
+- Eleanor Roosevelt`];
+
+let index = Math.floor(Math.random() * 3);
+
+console.log(quotes[index]);
